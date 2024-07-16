909703abfb59d9f6b2ed598573a9fdcbb38f125d
haxinh1 <hanguyen010129@gmail.com>
2024-07-16 12:25:49 +0700

    them case sua xóa

--- index.php
+++ index.php
@@ -1,12 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hheehe</h1>
-    <h1>sagdadas</h1>
-</body>
-</html>
+
+<?php
+
+
+
+    include "model/product.php";
+    include "model/category.php";
+    include "global.php";
+    include "controller/ControllerHome/HomeControl.php";
+    include "controller/ControllerHome/ProductControl.php";
+
+
+    
+    include "views/header.php";
+
+    if ((isset($_GET['act']))&&($_GET['act']!="")) {
+        $act = $_GET['act'];
+        switch ($act) {
+            case 'gioithieu':
+           
+                include "views/gioithieu.php";
+                break;
+            // -------------------------- User ------------------------------------------------------
+            case 'form_login':
+           
+                include "views/login.php";
+                break;
+            case 'login':
+                login();   
+                break;
+            case 'logout':
+                logout();
+                break;
+            case 'register':
+                include "views/signup.php";
+                break;
+            case 'signup':
+                signup();
+                break;
+
+           
+            default:
+                include "views/home.php";
+                break;
+            
+        }
+
+    }
+    include "views/footer.php";
+?>
